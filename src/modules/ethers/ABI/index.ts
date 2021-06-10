1bd6fdf8a319ef2fd0731974ce764cf8682c62d1
phwt.smwt <phwt.smwt@gmail.com>
2021-06-10 22:30:04 +0700

    refactor: import abi in json format

--- src/modules/ethers/ABI/index.ts
+++ src/modules/ethers/ABI/index.ts
@@ -1,4 +1,4 @@
-export * from "./DfiProtocols";
-export * from "./FairLaunch";
-export * from "./IPancakeRouter02";
-export * from "./IUniswapV2Pair";
+export { default as DFI_PROTOCOLS_ABI } from "./DfiProtocols.json";
+export { default as FAIRLAUNCH_ABI } from "./FairLaunch.json";
+export { default as IPANCAKE_ROUTER02_ABI } from "./IPancakeRouter02.json";
+export { default as IUNISWAPV2_PAIR_ABI } from "./IUniswapV2Pair.json";
