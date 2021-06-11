fbd3b7232179dd92ecb9cdcd426b3353b92b70e6
phwt.smwt <phwt.smwt@gmail.com>
2021-06-11 13:30:38 +0700

    refactor: move firebase config to env

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,17 @@ import PriceTitle from "./components/common/PriceTitle";
 import firebase from "firebase/app";
 import "firebase/analytics";
 import "firebase/performance";
-import firebaseConfig from "./firebaseConfig.json";
 
 const initializeFirebase = () => {
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
+  });
   firebase.analytics();
   firebase.performance();
 };
