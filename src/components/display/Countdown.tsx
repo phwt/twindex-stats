f66041bdedf9b315b3a414fa7c4d31d037085d57
phwt.smwt <phwt.smwt@gmail.com>
2021-07-01 12:30:29 +0700

    feat: responsive can unlock section

--- src/components/display/Countdown.tsx
+++ src/components/display/Countdown.tsx
@@ -34,6 +34,26 @@ const Countdown = () => {
     })();
   }, [address]);
 
+  const canUnlockBlock = (
+    <>
+      <h4 className="m-0">
+        <TextTransition inline text={canUnlock.amount} />
+      </h4>
+      <small className="text-muted">
+        (<TextTransition inline text={canUnlock.valueInUsd} />)
+      </small>
+      <br />
+      <small>
+        Available to Unlock{" "}
+        <IconTooltip
+          icon="info-circle"
+          placement="bottom"
+          text="The locked rewards will release linearly across one month within blocks 8763010 to 9627010"
+        />
+      </small>
+    </>
+  );
+
   return (
     <Card className="h-100">
       <Card.Body>
@@ -49,22 +69,19 @@ const Countdown = () => {
             <small>TWIN Locked</small>
           </Col>
 
-          <Col md={12} lg={6} className="text-center border-left">
-            <h4 className="m-0">
-              <TextTransition inline text={canUnlock.amount} />
-            </h4>
-            <small className="text-muted">
-              (<TextTransition inline text={canUnlock.valueInUsd} />)
-            </small>
-            <br />
-            <small>
-              Available to Unlock{" "}
-              <IconTooltip
-                icon="info-circle"
-                placement="bottom"
-                text="The locked rewards will release linearly across one month within blocks 8763010 to 9627010"
-              />
-            </small>
+          <Col
+            md={12}
+            lg={6}
+            className="text-center border-top mt-3 pt-3 d-inline d-lg-none"
+          >
+            {canUnlockBlock}
+          </Col>
+          <Col
+            md={12}
+            lg={6}
+            className="text-center border-left d-none d-lg-inline"
+          >
+            {canUnlockBlock}
           </Col>
         </Row>
       </Card.Body>
